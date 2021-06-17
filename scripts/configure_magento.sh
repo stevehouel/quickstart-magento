7c62c8f11be8e3b14cb0cfc41ec4b72a5a0d83f9
Steve Houël <steve.houel.perso@gmail.com>
2021-06-17 17:50:02 +0200

    Fixing issues

--- scripts/configure_magento.sh
+++ scripts/configure_magento.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 16 ]; then
-    echo $0: usage: configure_magento.sh dbhost dbuser dbpassword dbname cname adminfirstname adminlastname adminemail adminuser adminpassword cachehost protocol magentolanguage magentocurrency magentotimezone magentoversion
+if [ $# -ne 18 ]; then
+    echo $0: usage: configure_magento.sh dbhost dbuser dbpassword dbname cname adminfirstname adminlastname adminemail adminuser adminpassword cachehost protocol magentolanguage magentocurrency magentotimezone magentoversion magentousername magentopassword
     exit 1
 fi
 
@@ -27,17 +27,9 @@ magentousername=${17}
 magentopassword=${18}
 
 cd
-#curl -o magento.tar.gz $magentourl
-#echo "Running command aws s3 cp ${magentourl} magento.tar.gz"
-#aws s3 cp $magentourl magento.tar.gz
-#if [ $? -ne 0 ]; then
-#    echo "Error downloading media from s3"
-#    exit 1
-#fi
-[ -f "magento.tar.gz" ] && echo "Media file Found" || echo "Media file Not found"
 
 #Configure Magento credentials
-composer config --global http-basic.repo.magento.com $magentousername $magentopassword
+php /usr/local/bin/composer config --global http-basic.repo.magento.com $magentousername $magentopassword
 
 cd /var/www/html
 
@@ -59,119 +51,17 @@ else
   secure="--use-secure-admin=1 --use-secure=1 --base-url-secure=$protocol://$cname/"
 fi
 
+export PATH=$PATH:/var/www/html/bin
+
 cd /var/www/html/bin
+./magento module:disable {Magento_Elasticsearch,Magento_InventoryElasticsearch,Magento_Elasticsearch6,Magento_Elasticsearch7}
+
 ./magento setup:install --base-url=$protocol://$cname/ \
 --db-host=$dbhost --db-name=$dbname --db-user=$dbuser --db-password=$dbpassword \
 --admin-firstname=$adminfirst --admin-lastname=$adminlast --admin-email=$adminemail \
---admin-user=$adminuser --admin-password=$adminpassword --language=$magentolanguage \
---currency=$magentocurrency --timezone=$magentotimezone $secure
+--admin-user=$adminuser --admin-password=$adminpassword $secure
 
-init_value=`head -n8 /var/www/html/app/etc/env.php`
-
-cat << EOF > /var/www/html/app/etc/env.php
-$init_value
-  'session' =>
-  array (
-    'save' => 'redis',
-    'redis' =>
-      array (
-        'host' => '$cachehost',
-        'port' => '6379',
-        'password' => '',
-        'timeout' => '2.5',
-        'persistent_identifier' => '',
-        'database' => '0',
-        'compression_threshold' => '2048',
-        'compression_library' => 'gzip',
-        'log_level' => '1',
-        'max_concurrency' => '6',
-        'break_after_frontend' => '5',
-        'break_after_adminhtml' => '30',
-        'first_lifetime' => '600',
-        'bot_first_lifetime' => '60',
-        'bot_lifetime' => '7200',
-        'disable_locking' => '0',
-        'min_lifetime' => '60',
-        'max_lifetime' => '2592000',
-    ),
-  ),
-  'cache' =>
-  array(
-     'frontend' =>
-     array(
-        'default' =>
-        array(
-           'backend' => 'Cm_Cache_Backend_Redis',
-           'backend_options' =>
-           array(
-              'server' => '$cachehost',
-              'port' => '6379',
-              ),
-      ),
-      'page_cache' =>
-      array(
-        'backend' => 'Cm_Cache_Backend_Redis',
-        'backend_options' =>
-         array(
-           'server' => '$cachehost',
-           'port' => '6379',
-           'database' => '1',
-           'compress_data' => '0',
-         ),
-      ),
-    ),
-  ),
-  'db' =>
-  array (
-    'table_prefix' => '',
-    'connection' =>
-    array (
-      'default' =>
-      array (
-        'host' => '$dbhost',
-        'dbname' => '$dbname',
-        'username' => '$dbuser',
-        'password' => '$dbpassword',
-        'model' => 'mysql4',
-        'engine' => 'innodb',
-        'initStatements' => 'SET NAMES utf8;',
-        'active' => '1',
-      ),
-    ),
-  ),
-  'resource' =>
-  array (
-    'default_setup' =>
-    array (
-      'connection' => 'default',
-    ),
-  ),
-  'x-frame-options' => 'SAMEORIGIN',
-  'MAGE_MODE' => 'production',
-  'cache_types' =>
-  array (
-    'config' => 1,
-    'layout' => 1,
-    'block_html' => 1,
-    'collections' => 1,
-    'reflection' => 1,
-    'db_ddl' => 1,
-    'eav' => 1,
-    'customer_notification' => 1,
-    'full_page' => 1,
-    'config_integration' => 1,
-    'config_integration_api' => 1,
-    'translate' => 1,
-    'config_webservice' => 1,
-    'compiled_config' => 1,
-  ),
-  'install' =>
-  array (
-    'date' => 'Tue, 19 Jul 2016 15:44:17 +0000',
-  ),
-]
-;
-EOF
+./magento module:disable {Magento_Elasticsearch,Magento_InventoryElasticsearch,Magento_Elasticsearch6,Magento_Elasticsearch7}
 
 ./magento config:set dev/static/sign 1
 
@@ -182,16 +72,9 @@ then
 
 fi
 
-
-cd /var/www/html/bin
-./magento setup:install --base-url=$protocol://$cname/ \
+magento setup:install --base-url=$protocol://$cname/ \
 --db-host=$dbhost --db-name=$dbname --db-user=$dbuser --db-password=$dbpassword \
 --admin-firstname=$adminfirst --admin-lastname=$adminlast --admin-email=$adminemail \
---admin-user=$adminuser --admin-password=$adminpassword --language=$magentolanguage \
---currency=$magentocurrency --timezone=$magentotimezone $secure
-
-fi
+--admin-user=$adminuser --admin-password=$adminpassword $secure
 
 ./magento info:adminuri > /home/ec2-user/adminuri
-
-php /var/www/html/bin/magento setup:static-content:deploy
